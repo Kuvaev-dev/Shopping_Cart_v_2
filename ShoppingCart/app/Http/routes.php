2ee33485d351f27a0e5e8f01545b0782bd0946bf
Никита Куваев <likeaboss13371@gmail.com>
2021-12-14 20:20:56 +0200

    Routing & User Auth Add

--- ShoppingCart/app/Http/routes.php
+++ ShoppingCart/app/Http/routes.php
@@ -4,3 +4,28 @@ Route::get('/', [
     'users' => 'ProductController@GetIndex',
     'as' => 'product.index'
 ]);
+
+Route::get('/signup', [
+   'uses' => 'UserController@GetSignup',
+    'as' => 'user.signup'
+]);
+
+Route::post('/signup', [
+    'uses' => 'UserController@PostSignup',
+    'as' => 'user.signup'
+]);
+
+Route::get('/signin', [
+    'uses' => 'UserController@GetSignin',
+    'as' => 'user.signin'
+]);
+
+Route::post('/signin', [
+    'uses' => 'UserController@PostSignin',
+    'as' => 'user.signin'
+]);
+
+Route::post('/profile', [
+    'uses' => 'UserController@GetProfile',
+    'as' => 'user.profile'
+]);
---
+++ ShoppingCart/resources/views/user/profile.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="row">
+        <div class="col-mb-4 col-md-offset-4">
+
+        </div>
+    </div>
+@endsection
